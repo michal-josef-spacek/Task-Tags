75852ad234aaf0c399e15987a5d5bb23321cc56e
Michal Špaček <tupinek@gmail.com>
2015-01-13 22:51:15 +0100

    First version.

---
+++ Tags.pm
@@ -0,0 +1,52 @@
+package Task::Tags;
+
+# Pragmas.
+use strict;
+use warnings;
+
+# Version.
+our $VERSION = 0.01;
+
+1;
+
+__END__
+
+=pod
+
+=encoding utf8
+
+=head1 NAME
+
+Task::Tags - Install the Tags modules.
+
+=head1 SYNOPSIS
+
+ cpanm Task::Tags
+
+=head1 SEE ALSO
+
+L<Tags>,
+L<Tags::Element>,
+L<Tags::Output::PYX>.
+
+=head1 REPOSITORY
+
+L<https://github.com/tupinek/Taks-Tags>
+
+=head1 AUTHOR
+
+Michal Špaček L<mailto:skim@cpan.org>
+
+L<http://skim.cz>
+
+=head1 LICENSE AND COPYRIGHT
+
+ © 2014 Michal Špaček
+ Artistic License
+ BSD 2-Clause License
+
+=head1 VERSION
+
+0.01
+
+=cut
